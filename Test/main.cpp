7085cd936c739efbb1fa432141139576940c5aad
clarkehe <clarkehe@tencent.com>
2018-12-18 20:59:29 +0800

    macOS

---
+++ Test/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Clarke on 2018/12/18.
+//  Copyright © 2018 Clarke. All rights reserved.
+//
+
+#include <iostream>
+#include "../Math/Vector2D.h"
+
+int main(int argc, const char * argv[])
+{
+    Vector2D v;
+    v.x = 1;
+    v.y = 2;
+    
+    std::cout << v;
+    return 0;
+}
